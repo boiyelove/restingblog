97afc2f24af44adaa7b6630f97d3cf4c6ae462fd
Daahrmmie Boiyelove <daahrmmieboiye@gmail.com>
2016-05-06 06:20:29 -0700

    Added files via upload
    Task 2: Access Multiple Databases 
    1) The goal is to be able to access the following databases:
      - MySQL
      - PostgreSQL
      - Microsoft SQL Server
      - MongoDB
    2) The environment should be configurable where the developer can easily switch from QA (Testing) and Production (Live) environments. Each environment will point to a different database instance. I should be able to add additional environements such as a Dev environment for developing purposes.
    3) I would like to see a mechanism to load classes/ modules at the top of any python script that requires it. There should be a centralize location where I can add classes/ modules.
    4) I would like to be able to write custom classes and methods that accept dynamic arguments and be able to access them easily during any development.
    5) Create examples of simple and complex queries that can be performed for the following actions (GET, PUT, DELETE, POST, HEAD, OPTIONS) for all the databases

---
+++ restingblog/restingblog/settings/db_password.py
@@ -0,0 +1,11 @@
+#I've chose to store production database passwords here for secu=urity reasons
+#the file name can be changed but must be reflected in the settinfgs.production file
+#database password for firstdb
+firstdbpass = ''
+#database password for seconddb
+seconddbpass = ''
+#database password for thirddb
+thirddbpass = ''
+#database password for fourthdb
+forthdbpass = ''
+   
